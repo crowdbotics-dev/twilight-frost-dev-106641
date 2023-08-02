3acc2f57cb007c43fe787443e38db66a463e05f1
Crowdbotics <engineering@crowdbotics.com>
2023-08-02 23:22:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-frost-dev-106641",
+  url: "https://twilight-frost-dev-106641.botics.co",
+  api: "https://twilight-frost-dev-106641.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
